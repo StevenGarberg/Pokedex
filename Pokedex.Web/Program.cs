446d792e772e4ad0d441d32610296a2a77f55b07
Steven Garberg <steven.garberg@riviahealth.com>
2023-02-05 14:20:07 -0500

    Created initial PokeAPI calls on homepage

--- Pokedex.Web/Program.cs
+++ Pokedex.Web/Program.cs
@@ -1,5 +1,4 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using PokeApiNet;
 using Pokedex.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<PokeApiClient>();
 
 var app = builder.Build();
 
